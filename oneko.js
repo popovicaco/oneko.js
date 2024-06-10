4feb5a782c1a9e95d367a9ed0b2d0625b0456fe7
aco <74219208+popovicaco@users.noreply.github.com>
2024-06-09 21:43:27 -0400

    Update oneko.js

--- oneko.js
+++ oneko.js
@@ -96,7 +96,7 @@
     nekoEl.style.top = `${nekoPosY - 16}px`;
     nekoEl.style.zIndex = Number.MAX_VALUE;
 
-    let nekoFile = "https://popovicaco.github.io/oneko.js/oneko.gif"
+    let nekoFile = "https://popovicaco.github.io/oneko.js/neko.png"
     const curScript = document.currentScript
     if (curScript && curScript.dataset.cat) {
       nekoFile = curScript.dataset.cat
